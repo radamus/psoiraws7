b3e3233b7afffdb64896bfe7518bad7b54bafb0e
Radek Adamus <adamus.r@gmail.com>
2015-05-04 19:52:09 +0000

    preparing sample code

--- app.js
+++ app.js
@@ -1,84 +1,22 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+(function() 
+{
+	var helpers = require("./helpers");
+	var Queue = require("queuemanager");
+	var SQSCommand = require("./sqscommand");
+	var SQSConsole = require("./sqsconsole");
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+	var AWS_CONFIG_FILE = "./config.json";
+	var APP_CONFIG_FILE = "./app.json";
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+	var initConsole = function(AWS) {
+			var appConfig = helpers.readJSONFile(APP_CONFIG_FILE);
+			var queue = new Queue(new AWS.SQS(), appConfig.QueueUrl);
+			var sqsCommand = new SQSCommand(queue);
+			new SQSConsole(sqsCommand);
+	}
+		
+	
+	require("./awshelpers").initAWS(initConsole, AWS_CONFIG_FILE);
+		
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
+})()
